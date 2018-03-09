436e8f5a0b5026adcd3c1b2aa0b437e69eee45d7
Alicia Zaludova <aliciazaludova@gmail.com>
2018-03-08 22:16:19 -0600

    added bplate + basic stuff

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('can you see this?');
+
+// writeToDom("I go in div#2", "challenge-2");
+
+// writeToDom(domString, domId){
+// //   <DO STUFF HERE to put "I go in div#2" in the div is id="challenge-2">
+// }
